0f79b89f943988450c5de32ea2144531e1131633
graerook156 <graerook156@SDS-L-751HC63>
2024-09-25 10:48:54 -0400

    Add project files.

---
+++ CashRegister/Form1.Designer.cs
@@ -0,0 +1,350 @@
+﻿namespace CashRegister
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.selectionLabel = new System.Windows.Forms.Label();
+            this.recieptLabel = new System.Windows.Forms.Label();
+            this.toadFeetLabel = new System.Windows.Forms.Label();
+            this.toadFeetInput = new System.Windows.Forms.TextBox();
+            this.octopusLabel = new System.Windows.Forms.Label();
+            this.octopusEyelidsInput = new System.Windows.Forms.TextBox();
+            this.fairyLabel = new System.Windows.Forms.Label();
+            this.fairyTearsInput = new System.Windows.Forms.TextBox();
+            this.totalButton = new System.Windows.Forms.Button();
+            this.subtotalLabel = new System.Windows.Forms.Label();
+            this.subtotalOutput = new System.Windows.Forms.Label();
+            this.taxLabel = new System.Windows.Forms.Label();
+            this.taxOutput = new System.Windows.Forms.Label();
+            this.totalOutput = new System.Windows.Forms.Label();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.tenderedLabel = new System.Windows.Forms.Label();
+            this.tenderedInput = new System.Windows.Forms.TextBox();
+            this.changeButton = new System.Windows.Forms.Button();
+            this.changeLabel = new System.Windows.Forms.Label();
+            this.changeOutput = new System.Windows.Forms.Label();
+            this.recieptButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.BackColor = System.Drawing.Color.Purple;
+            this.titleLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.White;
+            this.titleLabel.Location = new System.Drawing.Point(-2, -1);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(394, 33);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Sorcerer Store";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // selectionLabel
+            // 
+            this.selectionLabel.BackColor = System.Drawing.Color.Purple;
+            this.selectionLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.selectionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.selectionLabel.ForeColor = System.Drawing.Color.White;
+            this.selectionLabel.Location = new System.Drawing.Point(12, 41);
+            this.selectionLabel.Name = "selectionLabel";
+            this.selectionLabel.Size = new System.Drawing.Size(145, 468);
+            this.selectionLabel.TabIndex = 1;
+            this.selectionLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // recieptLabel
+            // 
+            this.recieptLabel.BackColor = System.Drawing.Color.White;
+            this.recieptLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.recieptLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.recieptLabel.ForeColor = System.Drawing.Color.Black;
+            this.recieptLabel.Location = new System.Drawing.Point(173, 41);
+            this.recieptLabel.Name = "recieptLabel";
+            this.recieptLabel.Size = new System.Drawing.Size(206, 385);
+            this.recieptLabel.TabIndex = 2;
+            this.recieptLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // toadFeetLabel
+            // 
+            this.toadFeetLabel.AutoSize = true;
+            this.toadFeetLabel.BackColor = System.Drawing.Color.Purple;
+            this.toadFeetLabel.ForeColor = System.Drawing.Color.White;
+            this.toadFeetLabel.Location = new System.Drawing.Point(55, 52);
+            this.toadFeetLabel.Name = "toadFeetLabel";
+            this.toadFeetLabel.Size = new System.Drawing.Size(56, 13);
+            this.toadFeetLabel.TabIndex = 3;
+            this.toadFeetLabel.Text = "Toad Feet";
+            // 
+            // toadFeetInput
+            // 
+            this.toadFeetInput.Location = new System.Drawing.Point(55, 78);
+            this.toadFeetInput.Name = "toadFeetInput";
+            this.toadFeetInput.Size = new System.Drawing.Size(58, 20);
+            this.toadFeetInput.TabIndex = 4;
+            this.toadFeetInput.TextChanged += new System.EventHandler(this.toadFeetInput_TextChanged);
+            // 
+            // octopusLabel
+            // 
+            this.octopusLabel.AutoSize = true;
+            this.octopusLabel.BackColor = System.Drawing.Color.Purple;
+            this.octopusLabel.ForeColor = System.Drawing.Color.White;
+            this.octopusLabel.Location = new System.Drawing.Point(43, 115);
+            this.octopusLabel.Name = "octopusLabel";
+            this.octopusLabel.Size = new System.Drawing.Size(83, 13);
+            this.octopusLabel.TabIndex = 5;
+            this.octopusLabel.Text = "Octopus Eyelids";
+            // 
+            // octopusEyelidsInput
+            // 
+            this.octopusEyelidsInput.Location = new System.Drawing.Point(55, 143);
+            this.octopusEyelidsInput.Name = "octopusEyelidsInput";
+            this.octopusEyelidsInput.Size = new System.Drawing.Size(58, 20);
+            this.octopusEyelidsInput.TabIndex = 6;
+            this.octopusEyelidsInput.TextChanged += new System.EventHandler(this.octopusEyelidsInput_TextChanged);
+            // 
+            // fairyLabel
+            // 
+            this.fairyLabel.AutoSize = true;
+            this.fairyLabel.BackColor = System.Drawing.Color.Purple;
+            this.fairyLabel.ForeColor = System.Drawing.Color.White;
+            this.fairyLabel.Location = new System.Drawing.Point(55, 181);
+            this.fairyLabel.Name = "fairyLabel";
+            this.fairyLabel.Size = new System.Drawing.Size(59, 13);
+            this.fairyLabel.TabIndex = 7;
+            this.fairyLabel.Text = "Fairy Tears";
+            // 
+            // fairyTearsInput
+            // 
+            this.fairyTearsInput.Location = new System.Drawing.Point(56, 210);
+            this.fairyTearsInput.Name = "fairyTearsInput";
+            this.fairyTearsInput.Size = new System.Drawing.Size(58, 20);
+            this.fairyTearsInput.TabIndex = 8;
+            this.fairyTearsInput.TextChanged += new System.EventHandler(this.fairyTearsInput_TextChanged);
+            // 
+            // totalButton
+            // 
+            this.totalButton.Enabled = false;
+            this.totalButton.Location = new System.Drawing.Point(28, 245);
+            this.totalButton.Name = "totalButton";
+            this.totalButton.Size = new System.Drawing.Size(112, 29);
+            this.totalButton.TabIndex = 9;
+            this.totalButton.Text = "Calculate Total";
+            this.totalButton.UseVisualStyleBackColor = true;
+            this.totalButton.Click += new System.EventHandler(this.totalButton_Click);
+            // 
+            // subtotalLabel
+            // 
+            this.subtotalLabel.AutoSize = true;
+            this.subtotalLabel.BackColor = System.Drawing.Color.Purple;
+            this.subtotalLabel.ForeColor = System.Drawing.Color.White;
+            this.subtotalLabel.Location = new System.Drawing.Point(12, 286);
+            this.subtotalLabel.Name = "subtotalLabel";
+            this.subtotalLabel.Size = new System.Drawing.Size(46, 13);
+            this.subtotalLabel.TabIndex = 10;
+            this.subtotalLabel.Text = "Subtotal";
+            this.subtotalLabel.Click += new System.EventHandler(this.subtotalLabel_Click);
+            // 
+            // subtotalOutput
+            // 
+            this.subtotalOutput.AutoSize = true;
+            this.subtotalOutput.BackColor = System.Drawing.Color.Purple;
+            this.subtotalOutput.ForeColor = System.Drawing.Color.White;
+            this.subtotalOutput.Location = new System.Drawing.Point(88, 286);
+            this.subtotalOutput.Name = "subtotalOutput";
+            this.subtotalOutput.Size = new System.Drawing.Size(13, 13);
+            this.subtotalOutput.TabIndex = 11;
+            this.subtotalOutput.Text = "0";
+            // 
+            // taxLabel
+            // 
+            this.taxLabel.AutoSize = true;
+            this.taxLabel.BackColor = System.Drawing.Color.Purple;
+            this.taxLabel.ForeColor = System.Drawing.Color.White;
+            this.taxLabel.Location = new System.Drawing.Point(13, 310);
+            this.taxLabel.Name = "taxLabel";
+            this.taxLabel.Size = new System.Drawing.Size(25, 13);
+            this.taxLabel.TabIndex = 12;
+            this.taxLabel.Text = "Tax";
+            // 
+            // taxOutput
+            // 
+            this.taxOutput.AutoSize = true;
+            this.taxOutput.BackColor = System.Drawing.Color.Purple;
+            this.taxOutput.ForeColor = System.Drawing.Color.White;
+            this.taxOutput.Location = new System.Drawing.Point(88, 310);
+            this.taxOutput.Name = "taxOutput";
+            this.taxOutput.Size = new System.Drawing.Size(13, 13);
+            this.taxOutput.TabIndex = 13;
+            this.taxOutput.Text = "0";
+            // 
+            // totalOutput
+            // 
+            this.totalOutput.AutoSize = true;
+            this.totalOutput.BackColor = System.Drawing.Color.Purple;
+            this.totalOutput.ForeColor = System.Drawing.Color.White;
+            this.totalOutput.Location = new System.Drawing.Point(88, 340);
+            this.totalOutput.Name = "totalOutput";
+            this.totalOutput.Size = new System.Drawing.Size(13, 13);
+            this.totalOutput.TabIndex = 14;
+            this.totalOutput.Text = "0";
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.AutoSize = true;
+            this.totalLabel.BackColor = System.Drawing.Color.Purple;
+            this.totalLabel.ForeColor = System.Drawing.Color.White;
+            this.totalLabel.Location = new System.Drawing.Point(13, 340);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(31, 13);
+            this.totalLabel.TabIndex = 15;
+            this.totalLabel.Text = "Total";
+            // 
+            // tenderedLabel
+            // 
+            this.tenderedLabel.AutoSize = true;
+            this.tenderedLabel.BackColor = System.Drawing.Color.Purple;
+            this.tenderedLabel.ForeColor = System.Drawing.Color.White;
+            this.tenderedLabel.Location = new System.Drawing.Point(13, 374);
+            this.tenderedLabel.Name = "tenderedLabel";
+            this.tenderedLabel.Size = new System.Drawing.Size(53, 13);
+            this.tenderedLabel.TabIndex = 16;
+            this.tenderedLabel.Text = "Tendered";
+            // 
+            // tenderedInput
+            // 
+            this.tenderedInput.Location = new System.Drawing.Point(82, 371);
+            this.tenderedInput.Name = "tenderedInput";
+            this.tenderedInput.Size = new System.Drawing.Size(58, 20);
+            this.tenderedInput.TabIndex = 17;
+            this.tenderedInput.TextChanged += new System.EventHandler(this.tenderedInput_TextChanged);
+            // 
+            // changeButton
+            // 
+            this.changeButton.Enabled = false;
+            this.changeButton.Location = new System.Drawing.Point(28, 397);
+            this.changeButton.Name = "changeButton";
+            this.changeButton.Size = new System.Drawing.Size(112, 29);
+            this.changeButton.TabIndex = 18;
+            this.changeButton.Text = "Change";
+            this.changeButton.UseVisualStyleBackColor = true;
+            this.changeButton.Click += new System.EventHandler(this.changeButton_Click);
+            // 
+            // changeLabel
+            // 
+            this.changeLabel.AutoSize = true;
+            this.changeLabel.BackColor = System.Drawing.Color.Purple;
+            this.changeLabel.ForeColor = System.Drawing.Color.White;
+            this.changeLabel.Location = new System.Drawing.Point(22, 441);
+            this.changeLabel.Name = "changeLabel";
+            this.changeLabel.Size = new System.Drawing.Size(44, 13);
+            this.changeLabel.TabIndex = 19;
+            this.changeLabel.Text = "Change";
+            // 
+            // changeOutput
+            // 
+            this.changeOutput.AutoSize = true;
+            this.changeOutput.BackColor = System.Drawing.Color.Purple;
+            this.changeOutput.ForeColor = System.Drawing.Color.White;
+            this.changeOutput.Location = new System.Drawing.Point(88, 441);
+            this.changeOutput.Name = "changeOutput";
+            this.changeOutput.Size = new System.Drawing.Size(13, 13);
+            this.changeOutput.TabIndex = 20;
+            this.changeOutput.Text = "0";
+            // 
+            // recieptButton
+            // 
+            this.recieptButton.Enabled = false;
+            this.recieptButton.Location = new System.Drawing.Point(28, 468);
+            this.recieptButton.Name = "recieptButton";
+            this.recieptButton.Size = new System.Drawing.Size(112, 29);
+            this.recieptButton.TabIndex = 21;
+            this.recieptButton.Text = "Print Reciept";
+            this.recieptButton.UseVisualStyleBackColor = true;
+            this.recieptButton.Click += new System.EventHandler(this.recieptButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(392, 518);
+            this.Controls.Add(this.recieptButton);
+            this.Controls.Add(this.changeOutput);
+            this.Controls.Add(this.changeLabel);
+            this.Controls.Add(this.changeButton);
+            this.Controls.Add(this.tenderedInput);
+            this.Controls.Add(this.tenderedLabel);
+            this.Controls.Add(this.totalLabel);
+            this.Controls.Add(this.totalOutput);
+            this.Controls.Add(this.taxOutput);
+            this.Controls.Add(this.taxLabel);
+            this.Controls.Add(this.subtotalOutput);
+            this.Controls.Add(this.subtotalLabel);
+            this.Controls.Add(this.totalButton);
+            this.Controls.Add(this.fairyTearsInput);
+            this.Controls.Add(this.fairyLabel);
+            this.Controls.Add(this.octopusEyelidsInput);
+            this.Controls.Add(this.octopusLabel);
+            this.Controls.Add(this.toadFeetInput);
+            this.Controls.Add(this.toadFeetLabel);
+            this.Controls.Add(this.recieptLabel);
+            this.Controls.Add(this.selectionLabel);
+            this.Controls.Add(this.titleLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label selectionLabel;
+        private System.Windows.Forms.Label recieptLabel;
+        private System.Windows.Forms.Label toadFeetLabel;
+        private System.Windows.Forms.TextBox toadFeetInput;
+        private System.Windows.Forms.Label octopusLabel;
+        private System.Windows.Forms.TextBox octopusEyelidsInput;
+        private System.Windows.Forms.Label fairyLabel;
+        private System.Windows.Forms.TextBox fairyTearsInput;
+        private System.Windows.Forms.Button totalButton;
+        private System.Windows.Forms.Label subtotalLabel;
+        private System.Windows.Forms.Label subtotalOutput;
+        private System.Windows.Forms.Label taxLabel;
+        private System.Windows.Forms.Label taxOutput;
+        private System.Windows.Forms.Label totalOutput;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.Label tenderedLabel;
+        private System.Windows.Forms.TextBox tenderedInput;
+        private System.Windows.Forms.Button changeButton;
+        private System.Windows.Forms.Label changeLabel;
+        private System.Windows.Forms.Label changeOutput;
+        private System.Windows.Forms.Button recieptButton;
+    }
+}
+
---
+++ CashRegister/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CashRegister
+{
+    public partial class Form1 : Form
+    {
+        //global variables
+        double subtotal;
+        double total;
+        double tax;
+        double toadFeetPrice = 3.25;
+        double octopusEyelidPrice = 5.00;
+        double fairyTearPrice = 8.50;
+        int toadFeetQuantity = 0;
+        int octopusEyelidQuantity = 0;
+        int fairyTearQuantity = 0;
+        double tendered;
+        double change;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void subtotalLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void totalButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //define variables
+                toadFeetQuantity = Convert.ToInt16(toadFeetInput.Text);
+                octopusEyelidQuantity = Convert.ToInt16(octopusEyelidsInput.Text);
+                fairyTearQuantity = Convert.ToInt16(fairyTearsInput.Text);
+
+                //calculate variables
+                subtotal = (toadFeetQuantity * toadFeetPrice) + (octopusEyelidQuantity * octopusEyelidPrice) + (fairyTearQuantity * fairyTearPrice);
+                tax = (subtotal * 0.13);
+                total = (subtotal + tax);
+
+                //display values
+                subtotalOutput.Text = $"{subtotal.ToString("C")}";
+                taxOutput.Text = $"{tax.ToString("C")}";
+                totalOutput.Text = $"{total.ToString("C")}";
+            }
+            catch
+            {
+                subtotalOutput.Text = "ERROR";
+                taxOutput.Text = "ERROR";
+                totalOutput.Text = "ERROR";
+            }
+
+
+
+        }
+
+        private void changeButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //define variables
+                tendered = Convert.ToDouble(tenderedInput.Text);
+
+                change = (tendered - total);
+                changeOutput.Text = $"{change.ToString("C")}";
+                recieptButton.Enabled = true;
+
+                //if (tendered < total);
+                {
+                    //changeOutput.Text = "ERROR";
+                }
+        
+            }
+            catch
+            {
+                changeOutput.Text = "ERROR";
+            }
+        }
+
+        private void recieptButton_Click(object sender, EventArgs e)
+        {
+            recieptLabel.Text = $"\nRECIEPT\n";
+
+            
+        }
+
+        private void tenderedInput_TextChanged(object sender, EventArgs e)
+        {
+            changeButton.Enabled = true;
+        }
+
+        private void toadFeetInput_TextChanged(object sender, EventArgs e)
+        {
+            totalButton.Enabled = true;
+        }
+
+        private void octopusEyelidsInput_TextChanged(object sender, EventArgs e)
+        {
+            totalButton.Enabled = true;
+        }
+
+        private void fairyTearsInput_TextChanged(object sender, EventArgs e)
+        {
+            totalButton.Enabled = true;
+        }
+    }
+}
+            
